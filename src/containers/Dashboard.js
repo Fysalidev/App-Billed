5b18440dc51229f7e78d3e7a6a5a169bf9fed933
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-05-20 16:45:03 +0200

    Bug fixé

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -145,10 +145,18 @@ export default class {
       this.counter ++
     }
 
-    bills.forEach(bill => {
+    /* bills.forEach(bill => {
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
+    }) */
 
+    filteredBills(bills, getStatus(this.index)).forEach((bill) => {
+      $(`#open-bill${bill.id}`).click((e) =>
+        this.handleEditTicket(e, bill, bills)
+      );
+    });
+
+    console.log(bills)
+    
     return bills
 
   }
