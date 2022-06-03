f1838c0d40c5d3e4fac549d1d1855ac8d98036d0
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-06-03 14:22:41 +0200

    Mettre en place les test NewBills

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -17,34 +17,33 @@ describe("Given I am connected as an employee", () => {
       expect(newBillForm).toBeTruthy()
     })
   })
-
-  test("Then it should render 8 entries", () => {
-    const html = NewBillUI();
-    document.body.innerHTML = html;
-
-    const expenseTypeInput = screen.getByTestId("expense-type");
-    expect(expenseTypeInput).toBeTruthy();
-
-    const expenseNameInput = screen.getByTestId("expense-name");
-    expect(expenseNameInput).toBeTruthy();
-
-    const datePicker = screen.getByTestId("datepicker");
-    expect(datePicker).toBeTruthy();
-
-    const amountInput = screen.getByTestId("amount");
-    expect(amountInput).toBeTruthy();
-
-    const vatInput = screen.getByTestId("vat");
-    expect(vatInput).toBeTruthy();
-
-    const pctInput = screen.getByTestId("pct");
-    expect(pctInput).toBeTruthy();
-
-    const commentary = screen.getByTestId("commentary");
-    expect(commentary).toBeTruthy();
-
-    const fileInput = screen.getByTestId("file");
-    expect(fileInput).toBeTruthy();
-  });
-
 })
+
+test("Then it should render 8 entries", () => {
+  const html = NewBillUI();
+  document.body.innerHTML = html;
+
+  const expenseTypeInput = screen.getByTestId("expense-type");
+  expect(expenseTypeInput).toBeTruthy();
+
+  const expenseNameInput = screen.getByTestId("expense-name");
+  expect(expenseNameInput).toBeTruthy();
+
+  const datePicker = screen.getByTestId("datepicker");
+  expect(datePicker).toBeTruthy();
+
+  const amountInput = screen.getByTestId("amount");
+  expect(amountInput).toBeTruthy();
+
+  const vatInput = screen.getByTestId("vat");
+  expect(vatInput).toBeTruthy();
+
+  const pctInput = screen.getByTestId("pct");
+  expect(pctInput).toBeTruthy();
+
+  const commentary = screen.getByTestId("commentary");
+  expect(commentary).toBeTruthy();
+
+  const fileInput = screen.getByTestId("file");
+  expect(fileInput).toBeTruthy();
+});
